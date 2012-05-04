f24e7e7dfe3ea1d946d9cd45524b2c6f04a425a0
Romain Piel <romain.piel@gmail.com>
2012-05-04 12:14:33 +0200

    Updated demo

--- demo/src/com/ekito/simplekmldemo/SimpleKMLDemoActivity.java
+++ demo/src/com/ekito/simplekmldemo/SimpleKMLDemoActivity.java
@@ -16,51 +16,54 @@ import com.ekito.simpleKML.model.Kml;
 public class SimpleKMLDemoActivity extends Activity {
 
 	private final String TAG = "SimpleKMLDemoActivity";
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        new ParsingTask().execute("bdx_airsidearea.kml");
-    }
-    
-    private class ParsingTask extends AsyncTask<String, String, Kml> {
 
-    	Serializer kmlSerializer;
-    	
-    	public ParsingTask() {
-    		kmlSerializer = new Serializer();
-    	}
-    	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		new ParsingTask().execute("test.kml");
+	}
+
+	private class ParsingTask extends AsyncTask<String, String, Kml> {
+
+		Serializer kmlSerializer;
+
+		public ParsingTask() {
+			kmlSerializer = new Serializer();
+		}
+
 		@Override
 		protected Kml doInBackground(String... params) {
-	        Log.d(TAG, "read started");
-	        // this will create a Kml class based on the informations described in params[0] (assets/bdx_airsidearea.kml)
+			Log.d(TAG, "read started");
+			// this will create a Kml class based on the informations described in params[0] (assets/test.kml)
 			Kml kml = null;
-	        try {
-	            InputStream is = getResources().getAssets().open(params[0]);
-	           	kml = kmlSerializer.read(is);
+			try {
+				InputStream is = getResources().getAssets().open(params[0]);
+				Log.d(TAG, "parsing started");
+				kml = kmlSerializer.read(is);
+				Log.d(TAG, "parsing done");
 			} catch (IOException e) {
 				Log.e(TAG, e.getMessage());
 			} catch (Exception e) {
 				Log.e(TAG, e.getMessage());
 			}
-	        Log.d(TAG, "read done");
-	        
-	        Log.d(TAG, "write started");
-	        // this will output the KML to /data/data/com.ekito.simplekmldemo/example_out.kml
-	        File out = new File(getDir("assets", Context.MODE_PRIVATE), "example_out.kml");
-	        try {
-	        	
-				kmlSerializer.write(kml, out);
-			} catch (Exception e) {
-				Log.e(TAG, e.getMessage());
+			Log.d(TAG, "read done");
+
+			if (kml != null) {
+				Log.d(TAG, "write started");
+				// this will output the KML to /data/data/com.ekito.simplekmldemo/example_out.kml
+				File out = new File(getDir("assets", Context.MODE_PRIVATE), "test.kml");
+				try {
+					kmlSerializer.write(kml, out);
+				} catch (Exception e) {
+					Log.e(TAG, e.getMessage());
+				}
+				Log.d(TAG, "write done");
 			}
-	        Log.d(TAG, "write done");
-	        
+
 			return kml;
 		}
-    	
-    }
+
+	}
 }
